690f65be1c5d5a0b3ebac0e0cdccbeaf7488bd49
Sugumar-B <169170989+Sugumar-B@users.noreply.github.com>
2024-10-24 15:24:26 +0530

    updated files

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,206 +1,306 @@
+from datetime import date
 import numpy as np
-import pandas as pd
+import pickle
 import streamlit as st
-from joblib import load
-from streamlit_option_menu import option_menu
 
-st.set_page_config(layout="wide")
 
-def selling_price():
-    
-    st.write(' :white[Fill the below details to find Predicted Selling Price]')
+# Streamlit page custom design
 
-    
-    # Define dictionaries for mapping strings to integers
-    status_dict = {'Lost': 0, 'Won': 1, 'Draft': 7, 'To be approved': 6, 'Not lost for AM': 2, 'Wonderful': 8, 'Revised': 5, 'Offered': 4, 'Offerable': 3}
-    item_type_dict = {'W': 5, 'WI': 6, 'S': 3, 'Others': 1, 'PL': 2, 'IPL': 0, 'SLAWR': 4}
+def streamlit_config():
 
-    # custom style for prediction result text - color and position
+    # page configuration
+    st.set_page_config(page_title='Industrial Copper Modeling')
+
+    # page header transparent color
+    page_background_color = """
+    <style>
+
+    [data-testid="stHeader"] 
+    {
+    background: rgba(0,0,0,0);
+    }
+
+    </style>
+    """
+    st.markdown(page_background_color, unsafe_allow_html=True)
+
+    # title and position
+    st.markdown(f'<h1 style="text-align: center;">Industrial Copper Modeling</h1>',
+                unsafe_allow_html=True)
+
+
+
+# custom style for submit button - color and width
+
+def style_submit_button():
+
+    st.markdown("""
+                    <style>
+                    div.stButton > button:first-child {
+                                                        background-color: #367F89;
+                                                        color: white;
+                                                        width: 70%}
+                    </style>
+                """, unsafe_allow_html=True)
+
+
+
+# custom style for prediction result text - color and position
+
+def style_prediction():
+
+    st.markdown(
+            """
+            <style>
+            .center-text {
+                text-align: center;
+                color: #20CA0C
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+
+
+# user input options
+
+class options:
+
     country_values = [25.0, 26.0, 27.0, 28.0, 30.0, 32.0, 38.0, 39.0, 40.0, 77.0, 
                     78.0, 79.0, 80.0, 84.0, 89.0, 107.0, 113.0]
 
     status_values = ['Won', 'Lost', 'Draft', 'To be approved', 'Not lost for AM',
                     'Wonderful', 'Revised', 'Offered', 'Offerable']
+    status_dict = {'Lost':0, 'Won':1, 'Draft':2, 'To be approved':3, 'Not lost for AM':4,
+                'Wonderful':5, 'Revised':6, 'Offered':7, 'Offerable':8}
 
     item_type_values = ['W', 'WI', 'S', 'PL', 'IPL', 'SLAWR', 'Others']
+    item_type_dict = {'W':5.0, 'WI':6.0, 'S':3.0, 'Others':1.0, 'PL':2.0, 'IPL':0.0, 'SLAWR':4.0}
 
-    application_values = [2.0, 3.0, 4.0, 5.0, 10.0, 15.0, 19.0, 20.0, 22.0, 25.0, 26.0, 27.0, 28.0, 29.0, 38.0, 39.0, 
-                          40.0, 41.0, 42.0, 56.0, 58.0, 59.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 79.0, 99.0]
+    application_values = [2.0, 3.0, 4.0, 5.0, 10.0, 15.0, 19.0, 20.0, 22.0, 25.0, 26.0, 
+                        27.0, 28.0, 29.0, 38.0, 39.0, 40.0, 41.0, 42.0, 56.0, 58.0, 
+                        59.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 79.0, 99.0]
 
-    product_ref_values = [611728, 611733, 611993, 628112, 628117, 628377, 640400, 640405, 640665, 164141591, 164336407,
-                          164337175, 929423819, 1282007633, 1332077137, 1665572032, 1665572374, 1665584320, 1665584642, 
-                        1665584662, 1668701376, 1668701698, 1668701718, 1668701725, 1670798778, 1671863738, 1671876026, 
-                        1690738206, 1690738219, 1693867550, 1693867563, 1721130331, 1722207579]
-        
-    user_data = pd.DataFrame(columns=[['quantity tons', 'customer', 'country', 'status', 'item_type', 'application', 'thickness', 'width', 'product_ref']])
-    # Get input from users
-    with st.form('Regression'):
+    product_ref_values = [611728, 611733, 611993, 628112, 628117, 628377, 640400, 
+                        640405, 640665, 164141591, 164336407, 164337175, 929423819, 
+                        1282007633, 1332077137, 1665572032, 1665572374, 1665584320, 
+                        1665584642, 1665584662, 1668701376, 1668701698, 1668701718, 
+                        1668701725, 1670798778, 1671863738, 1671876026, 1690738206, 
+                        1690738219, 1693867550, 1693867563, 1721130331, 1722207579]
 
-        col1,col2,col3 = st.columns([0.5,0.1,0.5])
 
-        with col1:
 
-            quantity = st.number_input(label='Quantity Tons' ,min_value= 0.1, max_value= 6.9248)
-            country = st.selectbox(label='Country', options=country_values)
-            item_type = st.selectbox(label='Item Type', options=item_type_values)
-            thickness = st.number_input(label='Thickness', min_value=0.1, max_value=3.28154)
-            product_ref = st.selectbox(label='Product Ref', options=product_ref_values)
+# Get input data from users for both regression and classification methods
 
-        with col3:
+class prediction:
+
+    def regression():
+
+        # get input from users
+        with st.form('Regression'):
+
+            col1,col2,col3 = st.columns([0.5,0.1,0.5])
+
+            with col1:
+
+                
+                
+                quantity_log = st.text_input(label='Quantity Tons (Min: 0.00001 & Max: 1000000000)')
+
+                country = st.selectbox(label='Country', options=options.country_values)
+
+                item_type = st.selectbox(label='Item Type', options=options.item_type_values)
+
+                thickness_log = st.number_input(label='Thickness', min_value=0.1, max_value=2500000.0, value=1.0)
+
+                product_ref = st.selectbox(label='Product Ref', options=options.product_ref_values)
+
             
-            customer = st.number_input(label='Customer ID', min_value= 30147616 , max_value=30408185)
-            status = st.selectbox(label='Status', options=status_values)
-            application = st.selectbox(label='Application', options=application_values)
-            width = st.number_input(label='Width', min_value=700, max_value=1500)
-            st.write('')
-            st.write('')
-            button = st.form_submit_button(label='SUBMIT')
+            with col3:
+
+                
+                customer = st.text_input(label='Customer ID (Min: 12458000 & Max: 2147484000)')
+
+                status = st.selectbox(label='Status', options=options.status_values)
+
+                application = st.selectbox(label='Application', options=options.application_values)
+
+                width = st.number_input(label='Width', min_value=1.0, max_value=2990000.0, value=1.0)
+
+                st.write('')
+                st.write('')
+                button = st.form_submit_button(label='SUBMIT')
+                style_submit_button()
+
+
+        # give information to users
+        col1,col2 = st.columns([0.65,0.35])
+        with col2:
+            st.caption(body='*Min and Max values are reference only')
+
+
+        # user entered the all input values and click the button
+        if button:
             
-    col1,col2 = st.columns([0.65,0.35])
-    with col2:
-        st.caption(body='*Min and Max values are reference only')
-
-    # Process user input and make predictions
-    # Adjusted form submission check
-    if button:
-        # Convert quantity to string       
-        file_path = 'regression_model.joblib'  # Adjust the file path as per your saved model
-
-        # Load the saved model
-        loaded_model = load(file_path)
-        
-        # Convert status and item_type to their corresponding integer representations
-        status_int = status_dict.get(status)
-        item_type_int = item_type_dict.get(item_type)
-
-        # Make predictions
-        y_pred = loaded_model.predict(np.array([[quantity, customer, country, status_int, item_type_int, 
-                                application, thickness, width, product_ref]]))
-        selling_price = np.exp(y_pred[0])  # Apply exponential to get back original scale
-        
-        selling_price = round(selling_price,2)
-        
-        st.header(f'Predicted Selling Price is: {selling_price}')
-
-
-def status():
-    
-    st.write(' :white[Fill the below details to find Predicted Status]')
-    
-    # Define dictionaries for mapping strings to integers
-    item_type_dict = {'W': 5, 'WI': 6, 'S': 3, 'Others': 1, 'PL': 2, 'IPL': 0, 'SLAWR': 4}
-
-    # custom style for prediction result text - color and position
-    country_values = [25.0, 26.0, 27.0, 28.0, 30.0, 32.0, 38.0, 39.0, 40.0, 77.0, 78.0, 79.0, 80.0, 84.0, 89.0, 107.0, 113.0]
-
-    item_type_values = ['W', 'WI', 'S', 'PL', 'IPL', 'SLAWR', 'Others']
-
-    application_values = [2.0, 3.0, 4.0, 5.0, 10.0, 15.0, 19.0, 20.0, 22.0, 25.0, 26.0, 27.0, 28.0, 29.0, 38.0, 39.0, 
-                          40.0, 41.0, 42.0, 56.0, 58.0, 59.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 79.0, 99.0]
-
-    product_ref_values = [611728, 611733, 611993, 628112, 628117, 628377, 640400, 640405, 640665, 164141591, 164336407,
-                          164337175, 929423819, 1282007633, 1332077137, 1665572032, 1665572374, 1665584320, 1665584642, 
-                        1665584662, 1668701376, 1668701698, 1668701718, 1668701725, 1670798778, 1671863738, 1671876026, 
-                        1690738206, 1690738219, 1693867550, 1693867563, 1721130331, 1722207579]
-    
-    with st.form('Regression'):
-
-        col1,col2,col3 = st.columns([0.5,0.1,0.5])
-        with col1:
-
-            quantity = st.number_input(label='Quantity Tons' ,min_value= 0.1, max_value= 6.9248)
-            country = st.selectbox(label='Country', options=country_values)
-            item_type = st.selectbox(label='Item Type', options=item_type_values)
-            thickness = st.number_input(label='Thickness', min_value=0.1, max_value=3.28154)
-            product_ref = st.selectbox(label='Product Ref', options=product_ref_values)
-
-        with col3:
+            # load the regression pickle model
+            with open('D:/Data science projects/Industrial copper modeling/regression_model.pkl', 'rb') as f:
+                model = pickle.load(f)
             
-            customer = st.number_input(label='Customer ID', min_value= 30147616 , max_value=30408185)
-            selling_price = st.number_input(label='Selling_price', min_value=0 , max_value=2500)
-            application = st.selectbox(label='Application', options=application_values)
-            width = st.number_input(label='Width', min_value=700, max_value=1500)
-            st.write('')
-            st.write('')
-            button = st.form_submit_button(label='SUBMIT')
+            # make array for all user input values in required order for model prediction
+            user_data = np.array([[
+                                np.log(float(quantity_log)), 
+                                customer, 
+                                country, 
+                                options.status_dict[status], 
+                                options.item_type_dict[item_type], 
+                                application, 
+                                np.log(float(thickness_log)),
+                                width, 
+                                product_ref 
+                            
+                                ]])
             
-    col1,col2 = st.columns([0.65,0.35])
-    with col2:
-        st.caption(body='*Min and Max values are reference only')
+            # model predict the selling price based on user input
+            y_pred = model.predict(user_data)
+
+            # inverse transformation for log transformation data
+            selling_price = np.exp(y_pred[0])
+
+            # round the value with 2 decimal point (Eg: 1.35678 to 1.36)
+            selling_price = round(selling_price, 2)
+
+            return selling_price
+
+
+    def classification():
+
+        # get input from users
+        with st.form('Classification'):
+
+            col1,col2,col3 = st.columns([0.5,0.1,0.5])
+
+            with col1:
+
+                
+                
+                quantity_log = st.text_input(label='Quantity Tons (Min: 0.00001 & Max: 1000000000)')
+
+                country = st.selectbox(label='Country', options=options.country_values)
+
+                item_type = st.selectbox(label='Item Type', options=options.item_type_values)
+
+                thickness_log = st.number_input(label='Thickness', min_value=0.1, max_value=2500000.0, value=1.0)
+
+                product_ref = st.selectbox(label='Product Ref', options=options.product_ref_values)
+
+
+            with col3:
+
+                
+                
+                customer = st.text_input(label='Customer ID (Min: 12458000 & Max: 2147484000)')
+
+                selling_price_log = st.text_input(label='Selling Price (Min: 0.1 & Max: 100001000)')
+
+                application = st.selectbox(label='Application', options=options.application_values)
+
+                width = st.number_input(label='Width', min_value=1.0, max_value=2990000.0, value=1.0)
+
+                st.write('')
+                st.write('')
+                button = st.form_submit_button(label='SUBMIT')
+                style_submit_button()
+        
+        
+        # give information to users
+        col1,col2 = st.columns([0.65,0.35])
+        with col2:
+            st.caption(body='*Min and Max values are reference only')
+
+
+        # user entered the all input values and click the button
+        if button:
+            
+            # load the classification pickle model
+            with open('D:/Data science projects/Industrial copper modeling/classification_model.pkl', 'rb') as f:
+                model = pickle.load(f)
+            
+            # make array for all user input values in required order for model prediction
+            user_data = np.array([[ 
+                                np.log(float(quantity_log)), 
+                                customer, 
+                                country,  
+                                options.item_type_dict[item_type], 
+                                application, 
+                                np.log(float(thickness_log)),
+                                width, 
+                                product_ref,
+                                np.log(float(selling_price_log))
+                                
+                                ]])
+            
+            # model predict the status based on user input
+            y_pred = model.predict(user_data)
+
+            # we get the single output in list, so we access the output using index method
+            status = y_pred[0]
+
+            return status
+
+
+
+streamlit_config()
+
+tab1, tab2 = st.tabs(['PREDICT SELLING PRICE', 'PREDICT STATUS'])
+
+with tab1:
+
+    try:
     
-    # Process user input and make predictions
-    # Adjusted form submission check
-    if button:
-        # Convert quantity to string       
-        file_path = 'class_model.joblib'  # Adjust the file path as per your saved model
+        selling_price = prediction.regression()
 
-        # Load the saved model
-        loaded_model = load(file_path)
-        
-        # Convert status and item_type to their corresponding integer representations
-        item_type_int = item_type_dict.get(item_type)
-
-        # Make predictions
-        y_pred = loaded_model.predict(np.array([[quantity, customer, country, item_type_int, application, thickness, width, product_ref, selling_price]]))
-        
-        if y_pred[0] == 1:
-            st.header('Prediction status is "Won"')
-
-        else:
-            st.header('Predicted status is "Lose"')
-        
-        
-def home():
+        if selling_price:
+            # apply custom css style for prediction text
+            style_prediction()
+            st.markdown(f'### <div class="center-text">Predicted Selling Price = {selling_price}</div>', unsafe_allow_html=True)
+            #st.balloons()
     
-    st.markdown(
-        "<h1 style='text-align: center; color: pink;'>Copper Modeling Web Page</h1>",
-        unsafe_allow_html=True)
 
-    #st.markdown(f""" <style>.stApp {{
-     #                   background: url('https://ruthbroadbent.com/wp-content/uploads/2020/08/1RuthBroadbentCopperWireImaginedLines2017DetailInExhibition1Website-min.jpg');   
-      ##             </style> """,unsafe_allow_html=True)
+    except ValueError:
 
-    st.markdown("""
-                        <style>
-                        div.stButton > button:first-child {
-                                                            background-color: #367F89;
-                                                            color: white;
-                                                            width: 70%}
-                        </style>
-                    """, unsafe_allow_html=True)
+        col1,col2,col3 = st.columns([0.26,0.55,0.26])
+
+        with col2:
+            st.warning('##### Quantity Tons / Customer ID is empty')
+
+     
+
+with tab2:
+
+    try:
+
+        status = prediction.classification()
+
+        if status == 1:
+            
+            # apply custom css style for prediction text
+            style_prediction()
+            st.markdown(f'### <div class="center-text">Predicted Status = Won</div>', unsafe_allow_html=True)
+            #st.balloons()
+            
+
+        elif status == 0:
+            
+            # apply custom css style for prediction text
+            style_prediction()
+            st.markdown(f'### <div class="center-text">Predicted Status = Lost</div>', unsafe_allow_html=True)
+            st.snow()
     
-    
-    st.header(' :green[Importance of Machine Learning in Copper Pricing Prediction]')
-    st.write('### :violet[Introduction:]')
-    st.write('Welcome to the Copper Modeling Web Page! This platform utilizes machine learning algorithms') 
-    st.write('to provide valuable insights into copper pricing and transaction status.')
-    
-    st.write('### :violet[Importance of Machine Learning:]')
-    st.write('In the manufacturing domain, accurately predicting prices and transaction outcomes is paramount.') 
-    st.write('Traditional methods often struggle to account for the myriad of factors influencing copper prices and')
-    st.write('transaction statuses. This is where machine learning shines. By leveraging vast amounts of historical data and') 
-    st.write('sophisticated algorithms, machine learning can uncover hidden patterns and make highly accurate predictions.')
-    
-        
-    st.header(' :green[Use Case of This Project:]')
-    st.write('Our project focuses on two key aspects:')
 
-    st.write('### :violet[Selling Price Prediction:]')
-    st.write('Users can input various parameters related to copper transactions, and')
-    st.write('our machine learning model predicts the selling price. This empowers stakeholders to make informed')
-    st.write('decisions about pricing strategies and negotiations.')
-    st.write('### :violet[Status Prediction:]') 
-    st.write('Users provide transaction details, and our model predicts whether the transaction is likely to be') 
-    st.write('successful ("Won") or unsuccessful ("Lost"). This aids in risk assessment and resource allocation.')
-        
-        
-option = option_menu("", ['Home','Selling Price Prediction','Status Prediction'],default_index=0,orientation="horizontal")
+    except ValueError:
 
-if option == 'Home':
-    home()
-elif option == 'Selling Price Prediction':
+        col1,col2,col3 = st.columns([0.15,0.70,0.15])
 
-    selling_price()
-elif option == 'Status Prediction':
-
-    status()
+        with col2:
+            st.warning('##### Quantity Tons / Customer ID / Selling Price is empty')
